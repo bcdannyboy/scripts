81242d2cfa791ece96e8c868257b68fe7186719e
Daniel Bloom <daniel@bcdefense.com>
2021-03-18 11:25:02 -0400

    Update renamecvedirs.py

--- renamecvedirs.py
+++ renamecvedirs.py
@@ -3,25 +3,44 @@
 from glob import glob
 import os
 
-contents = glob("./*")
+windows = false
+
+if(windows):
+    contents = glob(".\\*")
+else:
+    contents = glob("./*")
 for fileordir in contents:
     if(os.path.isdir(fileordir)):
-        dirname = fileordir.split('./')[1]
+        if(windows):
+            dirname = fileordir.split('.\\')[1]
+        else:
+            dirname = fileordir.split('./')[1]
+            
+        nocve = 0
+        if('cve' in dirname):
+            cve_index = dirname.index('cve')
+        elif('CVE' in dirname)
+            cve_index = dirname.index('cve')
+        else:
+            print("no cve in dirname...")
+            nocve = 1
+
         
-        cve_index = dirname.index('CVE')
-
-        dirname = dirname[cve_index:]
-
-        finalcve = dirname[:9]
-
-        endofcve = dirname[9:]
-
-        for char in endofcve:
-            try:
-                charint = int(char)
-                finalcve = finalcve + char
-            except: # break at non-number char
-                break
+            
         
-        os.rename(fileordir, './' + finalcve)
-        
+        if(not nocve):
+            dirname = dirname[cve_index:]
+
+            finalcve = dirname[:9]
+
+            endofcve = dirname[9:]
+
+            for char in endofcve:
+                try:
+                    charint = int(char)
+                    finalcve = finalcve + char
+                except: # break at non-number char
+                    break
+            
+            os.rename(fileordir, './' + finalcve)
+            
